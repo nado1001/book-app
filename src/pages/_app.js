fc633e28a81598dd43ae5abecb487a020aeb330f
take-tech1001 <tk.tech1001@gmail.com>
2021-01-17 06:19:34 +0900

    Modfied global style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import React from "react";
+import { Provider } from "react-redux";
+import createStore from "../redux/store/store";
+import "../styles/globals.css";
+
+// const history = History.createBrowserHistory();
+export const store = createStore();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
