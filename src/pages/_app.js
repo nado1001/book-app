1c1c627b3d2d20e007ad1e2a71ebf16eb5167f13
take-tech1001 <tk.tech1001@gmail.com>
2021-01-18 01:05:27 +0900

    Supported material ui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,16 +2,46 @@ import React from "react";
 import { Provider } from "react-redux";
 import createStore from "../redux/store/store";
 import "../styles/globals.css";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../theme";
 
 // const history = History.createBrowserHistory();
 export const store = createStore();
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <React.Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    </React.Fragment>
   );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
