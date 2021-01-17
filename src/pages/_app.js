18aba397e326d0f7f5fd5a1329a7fcbbd4a0afe0
take-tech1001 <tk.tech1001@gmail.com>
2021-01-18 02:45:49 +0900

    Added Auth component to _app.jsx

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
+import Auth from "../components/organisms/Auth";
 
 // const history = History.createBrowserHistory();
 export const store = createStore();
@@ -29,12 +30,15 @@ export default function MyApp(props) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <title>Book Log</title>
       </Head>
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
+          <Auth>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </Auth>
         </ThemeProvider>
       </Provider>
     </React.Fragment>
