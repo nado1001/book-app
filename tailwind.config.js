2e9d2c1ea2adf4df5f567f3dcb4e1dc21e968fe1
take-tech1001 <tk.tech1001@gmail.com>
2021-01-27 01:48:48 +0900

    Modified tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         block02: "#333",
         secondary: "#46C9E5",
       },
+      width: {
+        30: "30%",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
   },
   variants: {
