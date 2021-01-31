c3faa1395d4c2031156dc9d4f30de15438df86b7
take-tech1001 <tk.tech1001@gmail.com>
2021-02-01 04:18:32 +0900

    Removed unnecessary parts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,24 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { TextInput } from "../components/atoms";
 import { Layout } from "../components/organisms";
 import { useState, useCallback, useEffect } from "react";
-import fetch from "node-fetch";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 
-export default function Home({ title, thumbnail }) {
+const useStyles = makeStyles({
+  Button: {
+    display: "flex",
+    margin: "1rem auto 0",
+    padding: 0,
+  },
+});
+
+export default function Home() {
   const [search, setSearch] = useState("");
   const [bookList, setBookList] = useState([]);
+  const classes = useStyles();
 
   const inputSearch = useCallback(
     (event) => {
@@ -36,62 +45,49 @@ export default function Home({ title, thumbnail }) {
   }, [search]);
 
   return (
-    <Layout className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout className="container">
+      <div className="md:w-3/6 w-4/6 mx-auto mt-4">
+        <h1 className="text-3xl">書籍検索</h1>
+        <p className="my-4">著者名、本のタイトルなどを入力してください</p>
+        <TextInput
+          fullWidth={true}
+          label={"検索"}
+          multiline={false}
+          required={true}
+          rows={1}
+          value={search}
+          type={"search"}
+          onChange={inputSearch}
+        />
+      </div>
 
-      <main className="py-20">
-        <div className="md:w-3/6 w-4/6 mx-auto mt-4">
-          <h1 className="text-3xl">書籍検索</h1>
-          <p className="my-4">著者名、本のタイトルなどを入力してください</p>
-          <TextInput
-            fullWidth={true}
-            label={"検索"}
-            multiline={false}
-            required={true}
-            rows={1}
-            value={search}
-            type={"search"}
-            onChange={inputSearch}
-          />
-        </div>
-
-        <ul className="bookList flex flex-wrap md:justify-center items-center justify-between">
-          {bookList.map((book, index) => (
-            <li className="bookList__item mt-8 md:w-30 w-1/2" key={index}>
-              <h2 className="text-center">{book.volumeInfo.title}</h2>
-              {book.volumeInfo.imageLinks ? (
-                <img
-                  src={book.volumeInfo.imageLinks.thumbnail}
-                  alt={book.volumeInfo.title}
-                  className="m-auto"
-                />
-              ) : (
-                <span className="flex justify-center items-center m-auto w-36 h-48">
-                  no image
-                </span>
-              )}
-            </li>
-          ))}
-        </ul>
-      </main>
+      <ul className="bookList flex flex-wrap md:justify-center items-center justify-between">
+        {bookList.map((book, index) => (
+          <li className="bookList__item mt-8 md:w-30 w-1/2" key={index}>
+            <h2 className="text-center mb-2">{book.volumeInfo.title}</h2>
+            {book.volumeInfo.imageLinks ? (
+              <img
+                src={book.volumeInfo.imageLinks.thumbnail}
+                alt={book.volumeInfo.title}
+                className="m-auto"
+              />
+            ) : (
+              <span className="flex justify-center items-center m-auto w-36 h-48">
+                no image
+              </span>
+            )}
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.Button}
+            >
+              <Link href={`/result/${book.id}`}>
+                <a className="px-8 py-2">詳細を見る</a>
+              </Link>
+            </Button>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
-
-// export async function getStaticProps() {
-//   const res = await fetch(
-//     "https://www.googleapis.com/books/v1/volumes?q=%E4%B8%89%E7%A7%8B%E7%B8%8B"
-//   );
-//   const json = await res.json();
-//   const title = json.items[0].volumeInfo.title;
-//   const thumbnail = json.items[0].volumeInfo.imageLinks.thumbnail;
-
-//   return {
-//     props: {
-//       title,
-//       thumbnail,
-//     },
-//   };
-// }
