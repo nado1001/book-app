9a6d9572b19c679c50a3da6c2839655b33b36998
take-tech1001 <tk.tech1001@gmail.com>
2021-02-19 00:33:07 +0900

    Fixed auth function

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -1,4 +1,4 @@
-import { signInAction, signOutAction } from "./actions";
+import { signInAction, signOutAction, booksUpdateAction } from "./actions";
 import Router from "next/router";
 import { auth, db, FirebaseTimestamp } from "../../firebase/index";
 
@@ -40,29 +40,35 @@ export const signIn = (email, password) => {
       return false;
     }
 
-    auth.signInWithEmailAndPassword(email, password).then((result) => {
-      const user = result.user;
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
 
-      if (user) {
-        const uid = user.uid;
-        db.collection("users")
-          .doc(uid)
-          .get()
-          .then((snapshot) => {
-            const data = snapshot.data();
+        if (user) {
+          const uid = user.uid;
+          db.collection("users")
+            .doc(uid)
+            .get()
+            .then((snapshot) => {
+              const data = snapshot.data();
 
-            dispatch(
-              signInAction({
-                isSignedIn: true,
-                uid: uid,
-                role: data.role,
-                username: data.username,
-              })
-            );
-            Router.push("/");
-          });
-      }
-    });
+              dispatch(
+                signInAction({
+                  isSignedIn: true,
+                  uid: uid,
+                  role: data.role,
+                  username: data.username,
+                })
+              );
+              Router.push("/");
+            });
+        }
+      })
+      .catch(() => {
+        alert("メールアドレスもしくはパスワードが正しくありません");
+        return false;
+      });
   };
 };
 
@@ -75,6 +81,11 @@ export const signUp = (username, email, password, confirmPassword) => {
       confirmPassword === ""
     ) {
       alert("必須項目が未入力です");
+      return false;
+    }
+
+    if (password.length < 6) {
+      alert("パスワードは６文字以上で入力してください");
       return false;
     }
 
@@ -138,5 +149,34 @@ export const resetPassword = (email) => {
           alert("登録されていないメールアドレスです。もう一度ご確認ください。");
         });
     }
+  };
+};
+
+export const updateBooks = (user) => {
+  return async (dispatch) => {
+    db.collection("users")
+      .doc(user)
+      .collection("books")
+      .get()
+      .then((snapshots) => {
+        let items = [];
+        snapshots.forEach((snapshot) => {
+          items.push(snapshot.data());
+        });
+        const unread = items.filter((item) => {
+          return item.progress === "unread";
+        });
+        const read = items.filter((item) => {
+          return item.progress === "read";
+        });
+        dispatch(
+          booksUpdateAction([
+            {
+              unread: unread,
+              read: read,
+            },
+          ])
+        );
+      });
   };
 };
